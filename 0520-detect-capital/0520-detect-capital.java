e62373ac3b79e3c242162b4ee23b33238a7db704
HARSH UPADHYAY <upadhyayharsh569@gmail.com>
2026-08-19 19:20:41 +0530

    Time: 1 ms (89.45%), Space: 43 MB (78.83%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+       int count = 0;
+        for (int i = 0;i<word.length();i++){
+            if (Character.isUpperCase(word.charAt(i))){
+            count++;
+           if  (count ==word.length() || count ==0) return true;}}
+           if (Character.isUpperCase(word.charAt(0)) && count == 1) return true;
+           if( count ==0 ){ return true;}
+            
+
+
+        
+        return false;
+        }
+}
